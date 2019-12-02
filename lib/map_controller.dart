499c4be48196b632996a714d7718dea2918cc293
synw <synw@yahoo.com>
2019-12-02 19:00:35 +0100

    Reorder imports

--- lib/map_controller.dart
+++ lib/map_controller.dart
@@ -3,4 +3,5 @@ library map_controller;
 export 'src/controller.dart';
 export 'src/models.dart';
 export 'src/tile_layer/layers.dart';
+export 'src/tile_layer/layers.dart';
 export 'src/tile_layer/tile_layers_bar.dart';
