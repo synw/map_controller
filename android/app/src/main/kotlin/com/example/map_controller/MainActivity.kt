9026795411edc02cc6a9ae367033df19623822a5
synw <ansterj@gmail.com>
2021-02-12 23:12:49 +0100

    Update project and dependencies

---
+++ android/app/src/main/kotlin/com/example/map_controller/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.map_controller
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ example/android/app/src/main/kotlin/com/example/map_controller_example/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.map_controller_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
